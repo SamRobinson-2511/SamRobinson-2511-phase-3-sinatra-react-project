87a8fea78d073e2dca4bee4150868fb14ddebd93
Julianne Huynh <JulianneHuynh@yahoo.com>
2023-01-05 17:10:05 -0600

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/collection" do
+#   # Add your routes here
+#   get "/collection" do
 
-    collection = Collection.all
-    collection.to_json
-  end
+#     collection = Collection.all
+#     collection.to_json
+#   end
 
-  get "/collector" do
-    collector = Collector.all
-    collector.to_json 
-  end
+#   get "/collector" do
+#     collector = Collector.all
+#     collector.to_json 
+#   end
 
-  get "/records" do
-    records = Record.all
-    records.to_json
-  end
+#   get "/records" do
+#     records = Record.all
+#     records.to_json
+#   end
 
-  get "/records" do
-      records = Record.all(param[:id])
-      records.to_json
-  end
+#   get "/records" do
+#       records = Record.all(param[:id])
+#       records.to_json
+#   end
 
-  delete '/records' do 
-    records = Record.find(param[:id])
-    records.destroy 
-    records.to_json
-  end
+#   delete '/records' do 
+#     records = Record.find(param[:id])
+#     records.destroy 
+#     records.to_json
+#   end
   
 end
 
